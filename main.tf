373e4f0add25237d15d03ed77a1e26881a958f72
Megha Pol <meghapol199@gmail.com>
2026-02-11 21:39:57 +0530

    KV

--- main.tf
+++ main.tf
@@ -32,3 +32,26 @@ resource "azurerm_subnet" "subnet" {
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = var.subnet_address_prefixes
 }
+
+
+#KV
+
+data "azurerm_client_config" "current" {}
+
+resource "azurerm_key_vault" "kv" {
+  name                        = var.key_vault_name
+  location                    = azurerm_resource_group.main_rg.location
+  resource_group_name         = azurerm_resource_group.main_rg.name
+  tenant_id                   = data.azurerm_client_config.current.tenant_id
+  sku_name                    = "standard"
+
+  soft_delete_retention_days  = 7
+  purge_protection_enabled    = false
+
+  enable_rbac_authorization   = true
+
+  tags = {
+    env   = "dev"
+    owner = "megha"
+  }
+}
--- variables.tf
+++ variables.tf
@@ -9,6 +9,9 @@ variable "vnet_address_space" { type = list(string) }
 variable "subnet_name"             { type = string }
 variable "subnet_address_prefixes" { type = list(string) }
 
+variable "key_vault_name" { type = string }
+ 
+
 # variable "tags" { type = map(string) }
 
 # variable "tags" {
